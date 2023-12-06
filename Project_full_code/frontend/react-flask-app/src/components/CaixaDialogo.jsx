e4e35c7e177af6742fb2b09d6d6c5c7b9b974fb6
Ricardo Fernandes Souza Lima <rifersouli1@gmail.com>
2023-12-05 21:34:38 -0300

    Correções finais
    Para o relatório

--- Project_full_code/frontend/react-flask-app/src/components/CaixaDialogo.jsx
+++ Project_full_code/frontend/react-flask-app/src/components/CaixaDialogo.jsx
@@ -21,7 +21,13 @@ const CaixaDialogo = ({ open = false, onClose, resumo }) => {
             >
                 <ModalClose></ModalClose>
                 <DialogTitle>Qual(is) das sentenças do resumo caracteriza(m) Objetivo?</DialogTitle>
-                <DialogContent>{resumo.resumo}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</DialogContent>
+                <DialogContent>{resumo.resumo}
+                    <p>Em indústrias de manufatura, um dos processos de produção consiste em cortar grandes objetos em peças menores.</p>
+                    <p>Em pesquisa operacional, o estudo desse processo é conhecido como o problema de corte de estoque (PCE) e, devido a sua dificuldade de resolução, métodos heurísticos vem sendo desenvolvidos pelos pesquisadores da área.</p>
+                    <p>Neste trabalho, propomos utilizar técnicas de análise de decisão multicritério (MCDA) a fim de resolver o PCE de forma eficiente em termos de GAP e tempo computacional.</p>
+                    <p>Nossa estratégia consiste em selecionar previamente os melhores padrões de corte com técnicas MCDA e resolver o problema apenas com esses padrões selecionados.</p>
+                    <p>Os testes computacionais mostraram que a solução obtida com essa estratégia apresenta um GAP entre 0,25% e 4,25% quando comparada com a solução ótima, dando indícios da qualidade da abordagem proposta.</p>
+                </DialogContent>
                 <button className='BotaoNenhumaAlternativa'>
                     <h3>Nenhuma das alternativas</h3>
                 </button>
